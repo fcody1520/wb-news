9776fff340b49e8f7cb684aec174df33f9d97ae4
Cody Farrington <fcody1520@gmail.com>
2024-01-09 15:52:32 -0700

    finished step 3

--- src/reducers/hackerNewsReducer.js
+++ src/reducers/hackerNewsReducer.js
@@ -1,10 +1,25 @@
-
+import axios from "axios"
 
 const initialState = {
     loading: false,
     articles: []
 }
 
+const requestArticles = async (dispatch) => {
+    dispatch({type: 'PENDING'})
+    let articles = await axios.get('/api/hacker-news')
+    .then(res => res.data)
+    dispatch({type: 'REQUEST_ARTICLES', payload: articles})
+}
+
+
 export default function hackerNewsReducer(state = initialState, action){
-    return state
+    switch(action.type){
+        case 'PENDING':
+            return {...state, loading: true}
+        case 'REQUEST_ARTICLES':
+            return {loading: false, articles: action.payload}
+        default:
+            return state; 
+    }
 }
